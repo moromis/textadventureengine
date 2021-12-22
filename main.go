8bf52c672a1099fe118ab81cdc3b92992ca6ddcc
Kevin Ulrich <kzulrich@gmail.com>
2021-12-21 18:01:31 -0800

    change styling of main window

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"textadventureengine/constants"
 	"textadventureengine/editor"
 	"textadventureengine/runner"
 	sharedUi "textadventureengine/shared/ui"
 
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
@@ -17,15 +19,28 @@ func main() {
 	// setup window
 	a := app.New()
 	w := a.NewWindow("Text Adventure Engine")
+	w.SetFixedSize(true)
+	w.Resize(fyne.NewSize(constants.WINDOW_WIDTH, constants.WINDOW_HEIGHT))
 	w.CenterOnScreen()
 
 	// HEADER
 	// buttons
+	title := widget.NewLabel("Text Adventure Engine")
 	openRunner := widget.NewButtonWithIcon("Open Game Runner", theme.MediaPlayIcon(), func() { go runner.OpenRunner(a, nil) })
 	openEditor := widget.NewButtonWithIcon("Open Game Editor", theme.MediaRecordIcon(), func() { go editor.OpenEditor(a) })
 
 	// set up the contents of the window
-	w.SetContent(container.NewVBox(sharedUi.ExitButton(w), layout.NewSpacer(), layout.NewSpacer(), openRunner, layout.NewSpacer(), openEditor, layout.NewSpacer(), layout.NewSpacer()))
+	w.SetContent(
+		container.NewVBox(
+			container.NewHBox(sharedUi.ExitButton(w)),
+			container.NewCenter(title),
+			layout.NewSpacer(),
+			container.NewCenter(openRunner),
+			container.NewCenter(openEditor),
+			layout.NewSpacer(),
+			layout.NewSpacer(),
+		),
+	)
 
 	// show and run the window
 	w.ShowAndRun()
